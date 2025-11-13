e34e37b0a420d371ef61a64a91222eb673a1ff7f
elizavetarybina <liza.rybina@inbox.ru>
2025-11-14 01:35:17 +0300

    init pages

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as FormPage } from './FormPage'
+export { default as ItemPage } from './ItemPage'
+export { default as ListPage } from './ListPage'
+export { default as NotFoundPage } from './NotFoundPage'
