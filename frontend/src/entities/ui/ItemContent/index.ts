db8184bdf1bf8130f72fb24efc028ffea96ec1e8
elizavetarybina <liza.rybina@inbox.ru>
2025-11-14 20:22:18 +0300

    added file for export

---
+++ frontend/src/entities/ui/ItemContent/index.ts
@@ -0,0 +1,3 @@
+export { default as AutoContent } from './AutoContent'
+export { default as RealEstateContent } from './RealEstateContent'
+export { default as ServiceContent } from './ServiceContent'
