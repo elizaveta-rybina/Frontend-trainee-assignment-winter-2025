4169e7d042549fb4cc69daf40f3d2eb9c8ce5aa4
elizavetarybina <liza.rybina@inbox.ru>
2025-11-15 14:43:14 +0300

    changes for testing the appearance of cards

--- frontend/src/pages/ListPage.tsx
+++ frontend/src/pages/ListPage.tsx
@@ -1,8 +1,16 @@
+import { PlaceAdButton } from '@/features/ui/PlaceAdButton/PlaceAdButton'
 import { ItemCardShowcase } from '@/widgets/ItemCardShowcase'
+import cls from './ListPage.module.scss'
 
 export const ListPage = () => {
 	return (
-		<div>
+		<div className={cls.listPage}>
+			<h1 className={cls.title}>Объявления</h1>
+
+			<div className={cls.buttonContainer}>
+				<PlaceAdButton />
+			</div>
+
 			<ItemCardShowcase />
 		</div>
 	)
--- frontend/src/widgets/ItemCardShowcase.tsx
+++ frontend/src/widgets/ItemCardShowcase.tsx
@@ -1,5 +1,5 @@
 import { ItemCard } from '@/entities/ui/ItemCard/ItemCard'
-import { Auto, ItemTypes, RealEstate, Service } from '@/shared'
+import { Auto, RealEstate, Service } from '@/shared'
 import React from 'react'
 import cls from './ItemCardShowcase.module.scss'
 import { mockAuto, mockRealEstate, mockService } from './cfg'
@@ -12,25 +12,19 @@ export const ItemCardShowcase: React.FC = () => {
 
 	return (
 		<div className={cls.showcase}>
-			<h1 className={cls.title}>ItemCard Component Showcase</h1>
-
 			<section className={cls.section}>
-				<h2 className={cls.sectionTitle}>Недвижимость</h2>
 				<ItemCard item={mockRealEstate} onAction={handleAction} />
 			</section>
 
 			<section className={cls.section}>
-				<h2 className={cls.sectionTitle}>Автомобиль</h2>
 				<ItemCard item={mockAuto} onAction={handleAction} />
 			</section>
 
 			<section className={cls.section}>
-				<h2 className={cls.sectionTitle}>Услуга</h2>
 				<ItemCard item={mockService} onAction={handleAction} />
 			</section>
 
 			<section className={cls.section}>
-				<h2 className={cls.sectionTitle}>С кастомной кнопкой</h2>
 				<ItemCard
 					item={mockRealEstate}
 					onAction={handleAction}
